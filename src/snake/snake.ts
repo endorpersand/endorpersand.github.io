1851434a3df1f47f7d3548eb501b52e8efbc761d
endrdragon <daendrdragon@gmail.com>
2022-02-21 18:50:09 -0500

    ts snake

--- src/snake/snake.ts
+++ src/snake/snake.ts
@@ -1,17 +1,15 @@
-let canvas = document.querySelector('#canvas');
-let ctx = canvas.getContext('2d');
+const canvas = document.querySelector('#canvas')! as HTMLCanvasElement;
+const ctx = canvas.getContext('2d')!;
 
 let s = 25; // 1 game pixel = s canvas pixels
-let toC = g => (g || [0,0]).map(x => x * s);             // converts a game value to canvas value
-let toG = c => (c || [0,0]).map(x => Math.floor(x / s)); // converts a canvas value to game value
-let arrEq = (a1, a2) => a1.every((x, i) => x == a2[i]);
-let includes = (metarr, arr) => metarr.some(x => arrEq(x, arr));
-let isOutBound = g => g[0] < 0 || g[0] >= gMaxWidth || g[1] < 0 || g[1] >= gMaxHeight; // game bounds [0, gMaxWidth), [0, gMaxHeight)
+
+type GameCoord = [number, number];
+type CanvasCoord = [number, number];
 let [gMaxWidth, gMaxHeight] = toG([canvas.width, canvas.height]);
-let snake = [[Math.floor(gMaxWidth / 4), Math.floor(gMaxHeight / 2)]]; // [head, ..., tail]
+let snake: GameCoord[] = [[Math.floor(gMaxWidth / 4), Math.floor(gMaxHeight / 2)]]; // [head, ..., tail]
 let facing = 'e'; //n, e, s, w
 
-let apple;
+let apple: GameCoord;
 
 // CONFIG //
 const pal = { // color palette
@@ -27,7 +25,7 @@ const tpu = 75; // ticks per update
 drawGRect([0,0], canvas.width, canvas.height, pal.bg, [0, 0]); // draw bg
 drawGRect(snake[0], s - 2, s - 2, pal.snake); // draw snake
 drawApple();
-let int = setInterval(update, tpu);
+let int: number | undefined = setInterval(update, tpu);
 
 document.onkeydown = e => {
     switch (e.code) {
@@ -73,13 +71,13 @@ document.onblur = () => {
 document.onfocus = () => {
     int = int || setInterval(update, tpu);
 }
-function drawGRect(g, cw, ch, color, cshift = [1,1]) {
-    let c = toC(g).map((x, i) => x + cshift[i]); // conv g coord and then shift point in c space
+function drawGRect(g: GameCoord, cw: number, ch: number, color: string, cshift = [1,1]) {
+    let c = toC(g).map((x, i) => x + cshift[i]) as CanvasCoord; // conv g coord and then shift point in c space
     ctx.fillStyle = color;
     ctx.fillRect(...c, cw, ch);
 }
 
-function drawSnakeSeg(g) {
+function drawSnakeSeg(g: GameCoord) {
     /*
      * if s = 4,
      * ........
@@ -118,7 +116,7 @@ function drawApple() {
 }
 
 function update() {
-    let nextPos = [...snake[0]]; //clone head
+    let nextPos: GameCoord = [...snake[0]]; //clone head
     switch (facing) {
         case 'n':
             nextPos[1]--;
@@ -133,14 +131,18 @@ function update() {
             nextPos[0]--;
             break;
     }
-    if(!arrEq(apple, nextPos)) drawGRect(snake.pop(), s + 2, s + 2, pal.bg, [-1, -1]); // if next pos is not apple, delete tail
+    if(!arrEq(apple, nextPos)) {
+        // if next pos is not apple, delete tail
+        let stail = snake.pop()!;
+        drawGRect(stail, s + 2, s + 2, pal.bg, [-1, -1]);
+    }
     if(arrEq(apple, nextPos)) drawApple(); // if next pos is apple, spawn a new apple
-    if(includes(snake, nextPos)) die(); // if next pos is snake, die
+    if(arrIncludes(snake, nextPos)) die(); // if next pos is snake, die
     if (isOutBound(nextPos)) die(); // if next pos is void, die
     //console.log(nextColor, appleData);
     snake.unshift(nextPos);
     drawSnakeSeg(nextPos);
-    document.querySelector('#score').textContent = `Score: ${snake.length}`;
+    document.querySelector('#score')!.textContent = `Score: ${snake.length}`;
 }
 
 function die() {
@@ -157,5 +159,26 @@ function die() {
     let button = document.createElement('button');
     button.textContent = 'Restart';
     button.onclick = () => location.reload();
-    document.querySelector('#score').parentElement.appendChild(button);
+    document.querySelector('#score')?.parentElement?.appendChild(button);
 }
+
+function toC(g: GameCoord | undefined): CanvasCoord {
+    // converts a game coord to canvas coord
+    return (g ?? [0, 0]).map(x => x * s) as CanvasCoord;
+}
+function toG(c: CanvasCoord | undefined): GameCoord {
+    // converts a canvas coord to game coord
+    return (c ?? [0, 0]).map(x => x / s) as GameCoord;
+}
+function isOutBound(g: GameCoord) {
+    // game bounds [0, gMaxWidth), [0, gMaxHeight)
+    return g[0] < 0 || g[0] >= gMaxWidth || g[1] < 0 || g[1] >= gMaxHeight;
+}
+
+function arrEq<T>(a1: T[], a2: T[]) {
+    return a1.every((x, i) => x == a2[i]);
+}
+function arrIncludes<T>(metarr: T[][], arr: T[]) {
+    return metarr.some(a => arrEq(a, arr));
+}
+export {}
