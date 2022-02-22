41845c0e1b2c001a264b8b616e7c5b4d138e8df2
endrdragon <daendrdragon@gmail.com>
2022-02-22 15:42:43 -0500

    rewrite boxes to fix center colors

--- src/index/boxes.ts
+++ src/index/boxes.ts
@@ -3,17 +3,11 @@ type RGB = [number, number, number];
 
 let wrapper = document.querySelector('.wrapper')!;
 let squares = [...document.querySelectorAll('.wrapper a')] as HTMLElement[];
+
 let cols = +getComputedStyle(wrapper).getPropertyValue('--cols');
-
 let rows = 3; // add more placeholder project boxes by changing this
-if (squares.length > cols * rows) rows = Math.ceil(squares.length / cols);
 
-let cornerPositions: Coord[] = [
-    [0, 0], 
-    [0, cols - 1], 
-    [rows - 1, 0], 
-    [rows - 1, cols - 1]
-];
+if (squares.length > cols * rows) rows = Math.ceil(squares.length / cols);
 
 for (let i = squares.length; i < (cols * rows); i++) {
     createPlaceholderSquare();
@@ -44,38 +38,16 @@ function createPlaceholderSquare() {
 }
 
 function regenColors() {
-    let corners: [Coord, RGB][] = Array.from({length: 4}, (_, i) => [cornerPositions[i], randRGB(0x50)])
+    let cornerClrs = Array.from({length: 4}, () => randRGB(0x50)) as [RGB, RGB, RGB, RGB];
+    setColors(cornerClrs);
+}
 
+function setColors(cornerClrs: [RGB, RGB, RGB, RGB]) {
     squares.forEach((s, i) => {
-        let pos = asCoord(i);
-        let clr: RGB = [0x77, 0x77, 0x77];
+        let [pr, pc] = asCoord(i);
+        let normPos = [pr / (rows - 1), pc / (cols - 1)] as [number, number];
+        let clr = interpolate(cornerClrs, normPos);
 
-        a: {
-            // if corner, use that color
-            for (let [cornerPos, cornerClr] of corners) {
-                if (manhattan(cornerPos, pos) == 0) {
-                    clr = cornerClr;
-                    break a;
-                }
-            }
-
-            // find all corners on the same row or col as pos
-            // if none, this must be an inner square, so all corners are part of its color calc
-            let calculatingCorners = corners.filter(c => {
-                let [[qx, qy], _] = c;
-                let [px, py] = pos;
-                
-                return px === qx || py === qy;
-            })
-            if (calculatingCorners.length === 0) calculatingCorners = corners;
-
-            // then find manhattan for each corner and interpolate
-            let clrs = calculatingCorners.map(([_, rgb]) => rgb);
-            let dist = calculatingCorners.map(([q, _]) => manhattan(pos, q));
-
-            clr = interpolate(clrs, dist);
-
-        }
         s.style.backgroundColor = rgb(clr);
 
         let hexText = s.querySelector('.colhex')!;
@@ -96,32 +68,52 @@ function rgb(arr: RGB) {
 }
 function hex(arr: RGB) {
     // converts rgb array => hex notation
-    return `#${arr.map(x => Math.round(x).toString(16)).join('')}`
+    return `#${arr.map(x => Math.round(x).toString(16).padStart(2, "0")).join('')}`
 }
 function asCoord(i: number): Coord {
     // takes an index in the array, maps it to its [row, col] value
     return [Math.floor(i / cols), i % cols];
 }
-function manhattan(p1: Coord, p2: Coord) {
-    let [x1, y1] = p1,
-        [x2, y2] = p2;
-    return Math.abs(x1 - x2) + Math.abs(y1 - y2)
-}
-function interpolate(clrs: RGB[], dist: number[]) {
-    // does weighted RMS interpolation between two colors
-    let totalDist = dist.reduce((acc, cv) => acc + cv, 0);
 
-    let channels: number[] = [];
-    for (let c = 0; c < 3; c++) { // iterate through all channels (R, G, B)
-        let colorvals = clrs.map(rgb => rgb[c]) // get channel values
-        let sum = colorvals.map((x, i) => (x ** 2) * (totalDist - dist[i])) // square each channel value, multiply by weight
-                           .reduce((acc, cv) => acc + cv, 0); // sum
+function zip<T, U>(t: T[], u: U[]): [T, U][] {
+    return t.map((t, i) => [t, u[i]]);
+}
+function lerp<T extends number[]>(pts: [T, T], dist: number): T {
+    let [p, q] = pts;
+    let length = p.length;
+    return Array.from({length}, (_, i) => {
+        let a = p[i], b = q[i];
+        return a + dist * (b - a);
+    }) as T;
+}
+
+function bilerp<T extends number[]>(pts: [T, T, T, T], dist: [number, number]): T {
+    type ArrPair = [T, T];
+    let [px, py] = dist;
+    let [top, bottom] = [pts.slice(0, 2) as ArrPair, pts.slice(2, 4) as ArrPair]
+    return lerp([lerp(top, px), lerp(bottom, px)], py);
+}
+
+function calcWeights(dist: [number, number]) {
+    return bilerp<[number, number, number, number]>([
+        [1,0,0,0],
+        [0,1,0,0],
+        [0,0,1,0],
+        [0,0,0,1]
+    ], dist);
+}
+
+function interpolate(clrs: [RGB, RGB, RGB, RGB], dist: [number, number]) {
+    let weights = calcWeights(dist);
+    return Array.from({length: 3}, (_, i) => {
+        let channels = clrs.map(clr => clr[i]);
+
+        let sqsum = zip(channels, weights)
+            .map(([c, w]) => w * c * c)
+            .reduce((acc, cv) => acc + cv);
         
-        let totalWeight = totalDist  * (colorvals.length - 1);
-        let normSum = Math.sqrt(sum / totalWeight);
-        channels.push(normSum);
-    }
-    return channels as RGB;
+        return Math.round(Math.sqrt(sqsum));
+    }) as RGB;
 }
 
 export {}; // recognize as module
