0ea4688a03a22ac334a1f69ad43d785214b4369b
endrdragon <daendrdragon@gmail.com>
2022-06-26 00:07:36 -0400

    add resources, further test with pixi

--- src/trainyard/frame.ts
+++ src/trainyard/frame.ts
@@ -1,10 +1,41 @@
 import * as PIXI from 'pixi.js'
+// @ts-ignore
+import assets from '../../static/*'
 
+const loader = PIXI.Loader.shared,
+   resources = PIXI.Loader.shared.resources;
+
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 const game = document.querySelector("#game")!;
 const app = new PIXI.Application({
-    width: 500, 
-    height: 500
+    width: 256, 
+    height: 256
 });
 
-app.renderer.backgroundColor = 0xFF0000;
+app.renderer.backgroundColor = 0xFFAFAF;
 game.prepend(app.view);
+
+loader
+    .add(assets["trainyard.json"])
+    .load(setup);
+
+let textures: {[name: string]: PIXI.Texture<PIXI.Resource>};
+
+let tile = (i: number) => [32 * (i % 8), 32 * (Math.floor(i / 8))] as [number, number];
+
+function setup() {
+    textures = resources[assets["trainyard.json"]].textures!;
+
+    let i = 0;
+    for (let t of Object.values(textures)) {
+        let st = new PIXI.Sprite(t);
+        st.position.set(...tile(i++));
+        app.stage.addChild(st);
+    }
+
+    app.ticker.add(gameLoop);
+};
+
+function gameLoop(delta: number) {
+    // cat.x = (cat.x + speed + delta) % 256;
+}
